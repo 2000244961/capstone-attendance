cf0aaf82f7332b5a0659f90729d12146f626b4eb
Jenesis <jenesisgracejoaquin9@gmail.com>
2025-11-07 09:25:55 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*',
+    origin: ['https://attendance-backend-4-gl1f.onrender.com/','http://localhost:7000'],
     methods: ['GET', 'POST']
   }
 });
