abf529229e617bc0b91e0f7ad4beff89cbc5304d
Jenesis <jenesisgracejoaquin9@gmail.com>
2025-11-12 02:31:31 +0800

    Fix: Ensure faceApiLoader.js filename matches import (case-sensitive)

--- attendance-system/src/features/students/pages/ManageStudent.js
+++ attendance-system/src/features/students/pages/ManageStudent.js
@@ -8,6 +8,7 @@ import { loadFaceApiModels, areModelsLoaded } from '../../shared/faceApiLoader';
 import { fetchStudents, addStudent, updateStudent, deleteStudent } from './studentApi';
 import { fetchSubjectSections } from './subjectSectionApi';
 import '../styles/ManageStudent.css';
+import { loadFaceApiModels, areModelsLoaded } from '../../shared/faceApiLoader';
 
 // Add processFaceDetection after all imports
 async function processFaceDetection(photo) {
