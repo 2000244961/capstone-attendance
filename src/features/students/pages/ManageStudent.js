0c0ee4d6ab09acdf379f6766113ce241e0845bc7
2000244961 <jenesisgracejoaquin9@gmail.com>
2025-11-14 20:53:03 +0800

    Update ManageStudent.js

--- src/features/students/pages/ManageStudent.js
+++ src/features/students/pages/ManageStudent.js
@@ -510,12 +510,18 @@ const ManageStudent = ({ refreshDashboard }) => {
               <option key={`form-section-${section}-${idx}`} value={section}>{section}</option>
             ))}
           </select>
-          <input
-            type="text"
-            placeholder="Grade Level"
-            value={formData.gradeLevel}
-            onChange={e => handleInputChange('gradeLevel', e.target.value)}
-          />
+              //changes here
+         <select
+          value={formData.gradeLevel}
+          onChange={e => handleInputChange('gradeLevel', e.target.value)}
+        >
+          <option value="">Select Grade Level</option>
+          {[1, 2, 3, 4, 5, 6].map(grade => (
+            <option key={grade} value={grade}>
+              Grade {grade}
+            </option>
+          ))}
+        </select>
           
           {/* Camera Section */}
           <div className="photo-section">
